7a138ffdce888fe2f9fac07b645439b0f50e3d36
GreenCobalt <snadol@purdue.edu>
2025-03-27 23:51:02 -0400

    don't recheck already checked

--- queue.c
+++ queue.c
@@ -1,7 +1,15 @@
 #include "queue.h"
 #include "tile_game.h"
 
+#include <stdlib.h>
+
 #define SOLVED_GAME 81985526993846272
+
+struct checked_node
+{
+    struct game_state state;
+    struct checked_node *next;
+};
 
 void enqueue(struct queue *q, struct game_state state)
 {
@@ -34,9 +42,60 @@ int check_win(struct game_state current)
     return ((serialize(current) - current.num_steps) == SOLVED_GAME);
 }
 
+void add_checked(struct checked_node **ll_head, struct game_state current)
+{
+    struct checked_node *new = (struct checked_node *)malloc(sizeof(struct checked_node));
+    new->state = current;
+    new->next = NULL;
+
+    if (*ll_head == NULL)
+    {
+        *ll_head = new;
+    }
+    else
+    {
+        struct checked_node *ll_current = *ll_head;
+        while (ll_current->next)
+            ll_current = ll_current->next;
+        ll_current->next = new;
+    }
+}
+
+int checked(struct checked_node **ll_head, struct game_state current)
+{
+    if (*ll_head == NULL)
+        return 0;
+    
+    struct checked_node *ll_current = *ll_head;
+    while (ll_current->next)
+    {
+        if (serialize(ll_current->state) - ll_current->state.num_steps == serialize(current) - current.num_steps)
+            return 1;
+        ll_current = ll_current->next;
+    }
+    return 0;
+}
+
+void free_checked(struct checked_node **ll_head)
+{
+    if (*ll_head == 0)
+        return;
+
+    struct checked_node *ll_current = *ll_head;
+    struct checked_node *ll_prev;
+
+    while (ll_current->next)
+    {
+        ll_prev = ll_current;
+        ll_current = ll_current->next;
+        free(ll_prev);
+    }
+}
+
 int number_of_moves(struct game_state start)
 {
     void (*tile_move[4])(struct game_state *state) = {move_down, move_up, move_right, move_left};
+    struct checked_node *checked_list = NULL;
 
     struct queue q;
     q.data.head = NULL;
@@ -45,14 +104,20 @@ int number_of_moves(struct game_state start)
     while (q.data.head)
     {
         struct game_state current = dequeue(&q);
+        if (checked(&checked_list, current))
+            continue;
+
         if (check_win(current))
         {
             printf("SOLVED in %d\n", current.num_steps);
             free_list(q.data);
+
+            free_checked(&checked_list);
             return current.num_steps;
         }
         else
         {
+            add_checked(&checked_list, current);
             for (int i = 0; i < 4; i++)
             {
                 struct game_state new = current;
